2c1bba194f1c1bc01f8dbfee51f06660c6309889
Horacio Andrade <hjap.94@gmail.com>
2023-09-01 05:58:21 -0400

    login

--- routes/User.routes.js
+++ routes/User.routes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getUsers, signup, deleteUser, updateUser, getUserById} = require('../controllers/User.controller');
+const { getUsers, signup, deleteUser, updateUser, getUserById, login} = require('../controllers/User.controller');
 const auth = require('../middleWares/auth');
 
 router.get ('/', getUsers);
 router.post('/',signup);
 router.put('/', updateUser)
 router.delete('/',deleteUser)
+router.post('/login',login)
+
 
 router.get('/:_id',auth,getUserById)
 
